2175ddd53efc3451097c9bc2ca9d2c266c0646d2
RuiZhao <Rui.Zhao@metropolia.fi>
2024-09-28 23:52:34 +0300

    Initial commit

--- travel.py
+++ travel.py
@@ -20,7 +20,7 @@ def get_coordinate(ident):
     return coordinate
 
 def get_distance(coordinate1, coordinate2):
-    return geopy.distance.distance(coordinate1, coordinate2)
+    return geopy.distance.distance(coordinate1, coordinate2).km
 
 ## Calculate the travelling time: 1 hour per 500 km
 
